b193b3ed07f23f473927c1dde867dc23bf33a2bb
jaevit17 <jaevit17@gmail.com>
2026-09-23 19:53:17 -0500

    Style: Rediseño Limpio Moderno, Responsive

--- ventas.js
+++ ventas.js
@@ -11,30 +11,16 @@ function calcularComision(numeroVenta,precioProducto){
 }
 function calcular(){
 
-    //recuperamos propiedades de las cajas de txt
-    let componenteSueldoBase=document.getElementById("txtSueldoBase");
-    let componenteNumeroVentas=document.getElementById("txtVentas");
-    let componentePrecio=document.getElementById("txtPrecio");
-
-    //recuperamos el valor de las cajas de texto
-    let sueldoBaseStr=componenteSueldoBase.value;
-    let numeroVentasStr=componenteNumeroVentas.value;
-    let precioProductoStr=componentePrecio.value;
-    
-    //convertimos el texto a numero
-    let sueldoBase=parseFloat(sueldoBaseStr);
-    let numeroVentas=parseFloat(numeroVentasStr);
-    let precioProducto=parseFloat(precioProductoStr);
+    let sueldoBase=recuperarFloat("txtSueldoBase");
+    let numeroVentas=recuperarFloat("txtVentas");
+    let precioProducto=recuperarFloat("txtPrecio");
 
     let comision=calcularComision(numeroVentas,precioProducto);
 
     let total=sueldoBase+comision;
 
-    let spSueldoBase=document.getElementById("spSueldoBase");
-    let spComision=document.getElementById("spComision");
-    let spTotal=document.getElementById("spTotal");
+    mostrarEnSpan("spSueldoBase",sueldoBase);
+    mostrarEnSpan("spComision",comision);
+    mostrarEnSpan("spTotal",total);
 
-    spSueldoBase.textContent=sueldoBase;
-    spComision.textContent=comision;
-    spTotal.textContent=total;
 } 
